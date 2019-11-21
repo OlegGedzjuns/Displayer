f92b6c51fb19255d60362db3d06dfec757c4ecf2
SecyrB <oleggedzjuns@gmail.com>
2019-11-21 23:33:37 +0200

    Simple stopwatch

--- 7Segment/7Segment.ino
+++ 7Segment/7Segment.ino
@@ -1,18 +1,20 @@
 #include "Displayer.h"
 
-void setup() 
+void setup()
 {
 	const short segPins[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
 	const short digPins[] = { 10, 11, 12 };
 	Displayer.Init(segPins, 3, digPins);
-	Displayer.Show("8.8.8.");
 }
 
 void loop() 
 {
 	Displayer.RefreshStart();
 
-	// You'r code here 
+	for (int i = 0; millis() % 1000 == 0; ++i)
+	{
+		Displayer.Show(i);
+	}
 
 	Displayer.RefreshEnd();
 }
