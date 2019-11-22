c07ae465acd2daf3d139f23ff8c5af70c37b9d34
SecyrB <oleggedzjuns@gmail.com>
2019-11-22 16:11:38 +0200

    Lexical errors corrected

--- 7Segment/7Segment.ino
+++ 7Segment/7Segment.ino
@@ -1,4 +1,4 @@
-// does all the calculations with temperature
+// Does all the calculations with temperature
 #include <DallasTemperature.h>
 // Unique 1-Wire interface requires only one port pin for communication with many devices
 // Power supply range is 3.0V to 5.5V
@@ -8,11 +8,11 @@
 
 #include "Displayer.h"
 
-// Setup a oneWire instance to communicate with any OneWire devices
+// Setup a 1-Wire manager to communicate with any 1-Wire devices
 constexpr auto ONE_WIRE_BUS = 13;
 OneWire OneWireManager(ONE_WIRE_BUS);
 
-// link Dallas Temperature with One Wire Manager
+// Link Dallas Temperature with One Wire Manager
 DallasTemperature Sensors(&OneWireManager);
 
 int timedCounter = 0;
@@ -25,11 +25,11 @@ void incrementNumber()
 
 void updateThermometer()
 {
-	// requests all possible devices on 1-wire, even if they are not connected
-	// the number of devices is hard coded in the header of the 1-wire library, standard value 15
+	// Requests all possible devices on 1-wire, even if they are not connected
+	// The number of devices is hard coded in the header of the 1-wire library, standard value 15
 	//Sensors.requestTemperatures();
 
-	// only requests from one device, many times faster
+	// Only requests from one device, many times faster
 	Sensors.requestTemperaturesByIndex(0);
 
 	Displayer.Show(Sensors.getTempCByIndex(0));
@@ -46,7 +46,7 @@ void setup()
 	Displayer.Initialize(segmentPins, sizeof digitPins / sizeof(short), digitPins, 60);
 }
 
-void loop() 
+void loop()
 {
 	// Simple timer
 	// NumberThread.check();
